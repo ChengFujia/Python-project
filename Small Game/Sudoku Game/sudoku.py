49d5301964570a60d570f5e0386d6dfbc877fed4
cfj <2218250273@qq.com>
2017-01-19 18:46:02 +0800

    2.use recusion to solve sudoku..

--- Small Game/Sudoku Game/sudoku.py
+++ Small Game/Sudoku Game/sudoku.py
@@ -79,5 +79,104 @@ def print_answers(board):
 		else:
 			print(spacer)
 
-Board = make_board()
-print_board(Board)
+def is_full(challange,m=3):
+	# Ending...
+	for i,j in itertools.product(range(m**2),repeat=2):
+		if challange[i][j] is None:
+			return False
+	return True
+
+def cal_candidate(challange,x,y,m=3):
+	# values to be filled..
+	candidate = range(1,m**2+1)
+	for i in range(m**2):
+		# row
+		if challange[x][i] in candidate:
+			candidate.remove(challange[x][i])
+		# column
+		if challange[i][y] in candidate:
+			candidate.remove(challange[i][y])
+	for i,j in itertools.product(range(m),repeat=2):
+		# start location in 3*3
+		x0,y0 = x-x%m,y-y%m
+		if challange[x0+i][y0+j] in candidate:
+			candidate.remove(challange[x0+i][y0+j])
+	return candidate
+
+def least_candidate(challange,m=3):
+	# find one having least candidates as beginner
+	least,x,y = m**2,-1,-1
+	for i,j in itertools.product(range(m**2),repeat=2):
+		if not challange[i][j]:
+			num = len(cal_candidate(challange,i,j))
+			if num < least:
+				least = num
+				x,y = i,j
+	return x,y
+
+def try_candidate(challange,id,m=3):
+	# ending..
+	if is_full(challange):
+		return challange
+
+	# (x,y) apart from id
+	x = id / (m**2)
+	y = id % (m**2)	
+	
+	# judge whether current is null or not
+	# find a null one
+	# +1 => first in row,then in column
+	while challange[x][y]:
+		id =  (id +1)%m**4
+		x = id/(m**2)
+		y = id%(m**2)
+	candidate = cal_candidate(challange,x,y)
+
+	# error or not
+	if len(candidate) == 0:
+		return False
+
+	# recusion
+	for i in range(len(candidate)):
+		challange[x][y] = candidate[i]
+		# next one
+		result_r = try_candidate(challange,(id+1)%m**4)
+		if not result_r:
+			# if next one == None, return False
+			# we try another one
+			pass
+		else:
+			# if next one == OK
+			# we return OK
+			return challange
+	# all candidates are not suitable,we return ERROR
+	else:
+		# Must return to what we are like before
+		challange[x][y] = None
+		return False
+
+def solving_sudoku(challange,m=3):
+	# judge whether is full
+	if is_full(challange):
+		return challange
+
+	# prepare for recusion --- find beginner
+	x,y = least_candidate(challange)
+
+	# to be convient, we use id to represent location
+	id = x *(m**2) + y
+
+	# try_candidate is our recusion function
+	result = try_candidate(challange,id)
+	return result
+
+if __name__ == "__main__":
+	Board = make_board()
+	challange = print_board(Board)
+	wait = raw_input("PRESS ENTER TO SHOW THE ANSWER.")
+	print('Raw Answer: ')
+	print_answers(Board)
+	print('Calculated from our algorithm: ')
+	result = solving_sudoku(challange)
+	print_answers(result)
+
