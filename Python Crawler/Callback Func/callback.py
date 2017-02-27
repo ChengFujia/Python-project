aa20fce75c442270856a47ad772074302f00a377
cfj <2218250273@qq.com>
2017-02-27 16:53:42 +0800

    1.基本原理：基于回调函数，实现事务处理

---
+++ Python Crawler/Callback Func/callback.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+Created on Mon Feb 27th 2017 12:13
+@author : David
+help : www.shiyanlou.com
+事件驱动－－回调函数实现爬虫
+"""
+
+"""
+## 非阻塞式IO，节省socket I/O阻塞时浪费的时间
+sock = socket.socket()
+sock.setblocking(False)
+# 不是傻傻的一直等待，而是试一下，不在就过一会再来
+try:
+	sock.connect(('xkcd.com',80))
+except BlockingIOError:
+	pass
+
+## 单线程上的多IO
+from selectors import DefaultSelector,EVENT_WRITE
+# C语言网络编程select函数－－不设置即处于阻塞状态，等待IO事件发生才被激活，然后告诉你socket上发生（读｜写｜异常）
+# Python中也有select函数，还有更高效的poll,还有简单更好用的DefaultSelector
+# 只需要将 非阻塞socket＋事件＋回调函数 绑定在一起就好
+selector = DefaultSelector()
+
+sock = socket.socket()
+sock.setblocking(False)
+try:
+	sock.connect(('xcsd.com',80))
+except BlockingIOError:
+	pass
+
+def connected():
+	selector.unregister(sock.fileno())
+	print('connected!')
+
+# selector.register在socket的写事件上绑定了回调函数connected
+# 该socket的 第一次 写事件标志着连接的建立，connected函数在连接建立成功后解除所有数据的绑定
+selector.register(sock.fileno(),EVENT_WRITE,connected)
+
+## 事件驱动
+def loop():
+	while True:	
+		events =selector.select()
+		# 不断获得IO事件
+		for event_key,event_mask in events:
+			# 遍历事件 并 调用相应处理（回调函数）
+			callback = event_key.data
+			callback()
+"""
